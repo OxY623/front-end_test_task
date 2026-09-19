36206ee3c0534e9af2cdee1dc948cdc907029d82
OxY623 <fuzzydoorproductions0@gmail.com>
2026-09-19 20:59:01 +0300

    feat: deployed

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/front-end_test_task/",
+  build: {
+    outDir: "dist",
+    target: "esnext",
+  },
   server: {
     port: 8081,
   },
